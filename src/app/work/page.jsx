74c01f3ba357d93044bb40c42577689de4dca91e
Odohofre <odohofrebright@gmail.com>
2023-09-26 18:23:18 +0100

    style: improve the hero section

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <main>
+      <h1>Work</h1>
+    </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,14 @@ import image from '../asset/portfolio.jpeg';
 export default function Hero() {
   function handleDownload() {}
   return (
-    <section className="flex flex-col items-center justify-center  h-[80vh] w-full px-6 pb-4">
+    <section className="flex flex-col items-center lg:justify-center  h-screen w-full px-6 pb-4">
       <div className="flex flex-col w-full items-center lg:flex-row lg:justify-between lg:items-center lg:space-x-[110px">
         <div className="flex-grow-0 order-1 lg:order-2 rounded-full overflow-hidden h-[243px] w-[243px] ">
           <Image src={image} width={243} height={243} alt="image of Bright" />
         </div>
         <div className="order-2 lg:order-1 flex-grow-0 text-center mt-8 lg:mt-0 max-w-sm">
           <h1 className="text-darkcolor text-[32px] lg:text-[44px] font-bold">
-            Hi, I am Bright, <br />A Web Developer
+            Hi, I am Bright, <br /> Web Developer
           </h1>
           <p className="mt-2 lg:mt-10 text-darkcolor">
             I create dynamic and exciting websites using cutting-edge tool. I
@@ -30,7 +30,7 @@ export default function Hero() {
       >
         <button
           type="button"
-          className="bg-primary text-white rounded-sm py-2 w-52 mt-3 lg:mt-9"
+          className="bg-primary text-white rounded-sm py-2 w-52 mt-8 lg:mt-9"
         >
           Download Resume
         </button>
